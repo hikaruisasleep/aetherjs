95019514ad800e84f3b33c3cc7e387bc2d951b39
Ben Wu <benayaj.2004@gmail.com>
2021-07-15 11:29:24 +0700

    update link cmd

--- commands/link.js
+++ commands/link.js
@@ -4,14 +4,39 @@ const links = require('../links.json');
 module.exports = {
     commands: 'jadwal',
     callback: (client, message, args, text) => {
+        if(!args) {
+            const linkList = [];
+
+            for (const linkSet in links) {
+                linkList.push(linkSet.lesson);
+            }
+
+            const embed = new Discord.MessageEmbed()
+                .setTitle('Link yang ada')
+                .setDescription(linkList);
+
+            message.channel.send(embed);
+        }
+
         for (const linkSet in links) {
-            if (linkSet.lesson === args) {
+            try {
+                if (linkSet.lesson === args) {
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle('Link zoom')
+                        .addField(`Link ${linkSet.lesson}`, linkSet.link);
+
+                    message.channel.send(embed);
+                    return;
+                } else {
+                    throw 'Pelajaran apa itu';
+                }
+            } catch (e) {
                 const embed = new Discord.MessageEmbed()
-                    .setTitle('Link zoom')
-                    .addField(`Link ${linkSet.lesson}`, linkSet.link);
+                    .setTitle('rusak')
+                    .setDescription('❌ kw habis ngapain pantek')
+                    .setFooter(e);
 
                 message.channel.send(embed);
-                return;
             }
         }
     },
