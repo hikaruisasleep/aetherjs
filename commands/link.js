c5be0a7017cf0feac3f9c566b5c8c562a533c266
Ben Wu <benayaj.2004@gmail.com>
2021-07-16 09:39:57 +0700

    fix univesal link parsing

--- commands/link.js
+++ commands/link.js
@@ -26,11 +26,10 @@ module.exports = {
         try {
             for (const linkSet of links) {
                 if (cleantext == linkSet.lesson.toLowerCase().replace(regex, '').replace(/ /g, '')) {
-                    const embed = new Discord.MessageEmbed()
-                        .setTitle('Link zoom');
+                    const embed = new Discord.MessageEmbed().setTitle('Link zoom');
 
-                    if (linkSet.lesson.toLowerCase().replace(regex, '').replace(/ /g, '') == "universal")
-                        embed.addField(`Link ABRAHAM IPA 2`, linkSet.link);
+                    if (linkSet.lesson.toLowerCase().replace(regex, '').replace(/ /g, '') == 'universal') {
+                        embed.addField('Link ABRAHAM IPA 2', linkSet.link);
                     } else {
                         embed.addField(`Link ${linkSet.lesson}`, linkSet.link);
                     }
