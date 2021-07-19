23f58f254c8cdb248815503e26769aa7a544bc93
Ben Wu <benayaj.2004@gmail.com>
2021-07-19 10:55:40 +0700

    logging lagi

--- commands/jadwal.js
+++ commands/jadwal.js
@@ -13,6 +13,8 @@ module.exports = {
                 console.log(table.day);
                 if(strftime('%A', date).toLowerCase() == table.day) {
                     for(const column of table.schedule) {
+                        console.log(strftime('%k', date).toLowerCase());
+                        console.log(column.cronTime.cronHour);
                         if((strftime('%k', date) == column.cronTime.cronHour) && column.lesson) {
                             return column.lesson;
                         } else {
@@ -28,9 +30,9 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .setTitle('Jadwal')
             .addFields(
-                { name: 'Pelajaran', value: currentLesson(), inline: false },
                 { name: 'Hari', value: `${strftime('%A', date)}`, inline: true },
                 { name: 'Jam', value: `${strftime('%H:%M', date)}`, inline: true },
+                { name: 'Pelajaran', value: currentLesson(), inline: false }
             )
             .setImage('https://cdn.discordapp.com/attachments/775253878312009775/864298701556285450/1626135257323.jpg');
 
